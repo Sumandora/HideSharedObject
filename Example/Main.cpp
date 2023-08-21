70948f6ba80cec3636b7e037df7d3cca9921dbde
Johannes <52950755+Sumandora@users.noreply.github.com>
2023-08-21 16:37:21 +0200

    Add github action, modernize CMake usage, update dependencies

--- Example/Main.cpp
+++ Example/Main.cpp
@@ -5,33 +5,36 @@
 
 #include <cassert>
 
-int callback(struct dl_phdr_info* info, size_t size, void* data) {
+int callback(struct dl_phdr_info* info, size_t size, void* data)
+{
 	printf("%s\n", info->dlpi_name);
 
 	assert(size == sizeof(*info));
-	assert(*(int*) data == 1337);
+	assert(*(int*)data == 1337);
 
 	return 0;
 }
 
 void* HideSharedObject::proxy;
 
-void InstallHideSharedObject() {
+void InstallHideSharedObject()
+{
 	HideSharedObject::AddHiddenSharedObject("libc.so");
 
-	void* functionPtr = (void*) dl_iterate_phdr;
+	void* functionPtr = (void*)dl_iterate_phdr;
 	size_t stolenBytes = 0;
 	while (stolenBytes <= DetourHooking::minLength) {
 		stolenBytes += ldisasm(static_cast<char*>(functionPtr) + stolenBytes, true);
 	}
 
-    DetourHooking::Hook* hook = new DetourHooking::Hook(functionPtr, (void*) HideSharedObject::HookFunc, stolenBytes);
-	hook->Enable();
+	DetourHooking::Hook* hook = new DetourHooking::Hook(functionPtr, (void*)HideSharedObject::HookFunc, stolenBytes);
+	hook->enable();
 
 	HideSharedObject::proxy = hook->trampoline;
 }
 
-int main() {
+int main()
+{
 	int data = 1337;
 	printf("Real:\n");
 	dl_iterate_phdr(callback, &data);
